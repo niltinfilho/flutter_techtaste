a4494ac4d330b980e8280a21623834227b7d1254
Nilton Filho <101483210+niltinfilho@users.noreply.github.com>
2025-04-15 21:51:52 -0300

    criando tela de splash

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_techtaste/ui/_core/app_theme.dart';
+import 'package:flutter_techtaste/ui/splash/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: AppTheme.appTheme, home: SplashScreen());
+  }
+}
